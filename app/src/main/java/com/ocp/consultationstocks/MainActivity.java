9b7a5df59918ae92e7450529c96822b82be54b7c
adiloffnen-coder <adil.offnen@gmail.com>
2026-09-14 23:35:54 +0100

    Update MainActivity.java

--- app/src/main/java/com/ocp/consultationstocks/MainActivity.java
+++ app/src/main/java/com/ocp/consultationstocks/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ocp.consultationstocks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View synthese = findViewById(R.id.btnSynthese);
+
+        if (synthese != null) {
+            synthese.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, StockSearchActivity.class);
+                startActivity(intent);
+            });
+        }
     }
 }
