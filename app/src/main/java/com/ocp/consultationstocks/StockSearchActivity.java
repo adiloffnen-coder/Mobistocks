213bb7224e0c00c518ac577ce789a606bc44caa6
adiloffnen-coder <adil.offnen@gmail.com>
2026-09-14 23:37:02 +0100

    Create StockSearchActivity.java

---
+++ app/src/main/java/com/ocp/consultationstocks/StockSearchActivity.java
@@ -0,0 +1,13 @@
+package com.ocp.consultationstocks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StockSearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_search);
+    }
+}
